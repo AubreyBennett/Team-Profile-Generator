7de6abef7414668cf6499b4d5870ae32c5683769
Aubrey Bennett <27aubreyb@gmail.com>
2021-02-27 09:57:31 -0500

    redownloaded node modules, rearranged files, finished employee.test tile-passed

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,33 +1,22 @@
-const Employee = require("./Employee");
+const Employee = require("../lib/Employee");
 
 describe("Employee", () => {
-    describe("print info", () => {
-        it("should", () => {
-        expect().toEqual();
+    describe("Initialization", () => {
+        it("should create an object with name id and email", () => {
+            const name = "will";
+            const id = "12";
+            const email = "sam@gmail.com"
+            const obj = new Employee(name, id, email);
+            expect(obj.name).toEqual(name);
+            expect(obj.id).toEqual(id);
+            expect(obj.email).toEqual(email);
         })
     })
 
-    describe("name", () => {
-        it("should", () => {
-        expect().toEqual();
-        })
-    })
-
-    describe("id", () => {
-        it("should", () => {
-        expect().toEqual();
-        })
-    })
-
-    describe("email", () => {
-        it("should", () => {
-        expect().toEqual();
-        })
-    })
-
-    describe("role", () => {
-        it("should", () => {
-        expect().toEqual();
+    describe("Role", () => {
+        it("should create an object with role", () => {
+            const obj = new Employee();
+            expect(obj.role).toEqual("Employee");
         })
     })
 })
